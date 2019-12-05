5d8546e4c5d57fa916986ff29c7c52db6f078aca
EncryptedEmu <birdsec@protonmail.com>
2019-12-04 18:50:04 -0600

    hello birds renders

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const wildcardSubdomains = require('wildcard-subdomains');
+const helmet = require('helmet');
+const express_enforces_ssl = require('express-enforces-ssl');
+const exphbs = require('express-handlebars');
+const helpers = require('handlebars-helpers')();
+
+let app = express();
+
+if (process.env.PRODUCTION) {
+  app.enable('trust proxy') // this is necessary as heroku is under a proxy
+  app.use(express_enforces_ssl());
+  app.use(helmet());
+}
+
+let hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: {
+    ifeq: function(a, b, options) {
+      if (a === b) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    },
+    toJSON : function(object) {
+      return JSON.stringify(object);
+    }
+  }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(wildcardSubdomains({
+  namespace: 's',
+  whitelist: ['www', 'app', 'api']
+}));
+
+app.get('/', function(req,res){
+  res.render('index')
+})
+
+app.get('/s/:subdomain', function(req,res){
+  res.render('index')
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
